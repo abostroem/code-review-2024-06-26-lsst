3deeadd8ff2d8057af5008dd91b9be8aa1096057
miguellloreto <migueldeloreto@gmail.com>
2024-06-26 15:19:19 -0300

    myplot changes

--- myplot.py
+++ myplot.py
@@ -3,12 +3,14 @@ import matplotlib
 import os
 from astropy.io import fits
 import glob
+
 data = np.ones([1024, 1024])
 data[500:600, 200:300] = 0
 col1 = data[20, :]
 col2 = data[200, :]
 col3 = data[800, :]
-pyplot.figure()
+
+g1= pyplot.figure()
 pyplot.plot(np.arange(1024), col1)
 mean = np.mean(col1)
 pyplot.axhline(mean)
@@ -16,7 +18,8 @@ pyplot.legend(['Column 20', 'Mean'])
 pyplot.title('Plot of Column 20 Values and Mean')
 pyplot.xlabel('Pixels')
 pyplot.ylabel('Intensity')
-pyplot.figure()
+
+g2=pyplot.figure()
 pyplot.plot(np.arange(1024), col2)
 mean = np.mean(col2)
 pyplot.axhline(mean)
@@ -24,7 +27,8 @@ pyplot.legend(['Column 200', 'Mean'])
 pyplot.title('Plot of Column 200 Values and Mean')
 pyplot.xlabel('Pixels')
 pyplot.ylabel('Intensity')
-pyplot.figure()
+
+g3=pyplot.figure()
 pyplot.plot(np.arange(1024), col3)
 mean = np.mean(col3)
 pyplot.axhline(mean)
